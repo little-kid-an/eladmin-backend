f447c253d8bcacac4b39c0589b2d2a7916da611c
little-kid-an <2494325435@qq.com>
2022-05-11 10:12:50 +0800

    1.初始化项目

---
+++ src/main/java/com/gxa/EladminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gxa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EladminBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EladminBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxa/EladminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EladminBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
